3d6d1fb7f2a06a177d0fc4bbd5ffe116a2faf679
Colonna, Olivier <olivier.colonna@accenture.com>
2016-01-20 00:48:15 +0000

    INDEX: initial commit & setup

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @AUTHORS oliviercolonna@gmail.com
+ * Copyright(c) 2016 Olivier Colonna
+ * MIT Licensed
+ */
+
+import React from 'react';
+
+class ReactVideo extends React.Component {
+
+};
+
+export default ReactVideo;
